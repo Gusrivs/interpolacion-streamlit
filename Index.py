d12a7425e6199444a6ce7b1f72580c1273728f60
gustavo <rt24007@ues.edu.sv>
2026-05-14 06:07:08 -0600

    Cambie mensaje de succes en inicio

---
+++ Index.py
@@ -0,0 +1,10 @@
+#This will be a practice 
+def Index(num1, num2):
+    resp = num1 + num2
+    print (f"Your answer is: {resp}")
+
+print("Calculator")
+num1 = int(input("num1: "))    
+num2 = int(input("num2: "))  
+Index(num1, num2)
+print("Thank you for using, have a good day!")
